78703cdfb3633496bf70afcb1861c762ce87f483
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-08-30 17:49:40 +0530

    update Article.js

--- main_backend/models/Article.js
+++ main_backend/models/Article.js
@@ -9,6 +9,16 @@ const ArticleSchema = new mongoose.Schema({
   source: String,
   date: String,
   time: String,
+  likes: { type: Number, default: 0 },
+  likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  shares: { type: Number, default: 0 },
+  saveCount: { type: Number, default: 0 },
+  comments: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    text: String,
+    createdAt: { type: Date, default: Date.now }
+  }],
+  expireAt: { type: Date } // Replaces created_at TTL. Unset this when saved.
 });
 
 module.exports = mongoose.model("Article", ArticleSchema, "serving_articles");
